ad2c13a267f9f3550501fac0e489fe73043eb97d
matt-churcher <91701502+matt-churcher@users.noreply.github.com>
2021-10-01 17:57:26 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://hello.churcher.me');
+  sleep(1);
+}
