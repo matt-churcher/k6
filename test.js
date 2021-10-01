ed7117771e22558770eaf61e8cc760ceb69b9764
matt-churcher <91701502+matt-churcher@users.noreply.github.com>
2021-10-01 21:06:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -5,11 +5,16 @@ export const options = {
   duration: '1m',
   vus: 50,
   thresholds: {
-    http_req_duration: ['p(95)<5000'],
+    http_req_duration: ['p(95)<1000'],
   },
 };
 
 export default function () {
   const res = http.get('http://hello.churcher.me');
+  
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
+  
   sleep(1);
 }
