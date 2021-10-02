619a2ec09a748ed8e3b8b18b5b410a7ea2a74336
matt-churcher <91701502+matt-churcher@users.noreply.github.com>
2021-10-02 22:29:47 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,24 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
-export const options = {
-  duration: '1m',
-  vus: 50,
+export let options = {
   thresholds: {
-    http_req_duration: ['p(95)<1000'],
+    http_req_failed: ['rate<0.01'],   // http errors should be less than 1% 
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
   },
+  stages: [
+    { duration: '2m', target: 500 },
+  ],
 };
 
 export default function () {
+
   const res = http.get('http://hello.churcher.me');
-  
-  check(result, {
-    'http response status code is 200': result.status === 200,
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
   });
-  
-  sleep(1);
+
+  sleep(1)
+
 }
