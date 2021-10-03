b6635e475c353d75699e0ae99b78cea37c4e85fa
matt-churcher <91701502+matt-churcher@users.noreply.github.com>
2021-10-03 21:13:13 +0100

    Create local.js

---
+++ local.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+  thresholds: {
+    http_req_failed: ['rate<0.1'],   // http errors should be less than 1% 
+    http_req_duration: ['p(95)<1000'], // 95% of requests should be below 200ms
+  },
+  stages: [
+    { duration: '1m', target: 100 },
+  ],
+};
+
+export default function () {
+
+  const res = http.get('http://localhost:5000');
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+
+  sleep(1)
+
+}
